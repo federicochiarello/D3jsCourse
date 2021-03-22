0d2dccf8ed22ec137097df7452d557b463ca928a
Federico <federicochiarello99@gmail.com>
2021-03-22 01:13:50 +0100

    React face refactoring

--- face/src/index.js
+++ face/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BackgroundCircle } from './BackgroundCircle'
+import { Eyes } from './Eye'
+import { Mouth } from './Mouth'
+
+const width = 960;
+const height = 500;
+const centerX = width /2;
+const centerY = height /2;
+const strokeWidth = 20;
+const eyeOffsetX = 90;
+const eyeOffsetY = 100;
+const eyeRadius = 50;
+const mouthWidth = 20;
+const mouthRadius = 140;
 
 const App = () => (
-    <h1>Hello JSK</h1>
+    <svg width={width} height={width}>
+        <g transform={`translate(${centerX},${centerY})`}>
+            <BackgroundCircle radius={centerY - strokeWidth /2} strokeWidth={strokeWidth} />
+            <Eyes eyeOffsetX={eyeOffsetX} eyeOffsetY={eyeOffsetY} eyeRadius={eyeRadius} />
+            <Mouth mouthRadius={mouthRadius} mouthWidth={mouthWidth} />
+        </g>
+    </svg>
 );
 
 const rootElement = document.getElementById('root');
